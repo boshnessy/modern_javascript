02bd81a2e2492ae51a8319cbdfe10baf8fbcc97a
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-04-23 21:31:24 -0500

    add numeral package

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+var moment = require('moment');
+var numeral = require('numeral');
+
 console.log("hi");
 console.log(moment("20150428", "YYYYMMDD").fromNow());
+
+
+console.log(numeral('3.467TB'));
